96b5e98e300353c7137e25ec539d5b3ba123eff3
Nagalakshman Chirukuri <chirukurinagalakshman@outlook.com>
2020-03-10 19:58:20 +0000

    Inline template in Angular

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  templateUrl: './user.component.html',
+  template: `<h1 class="text-center">Welcome to Angular</h1>
+              <h2 class="text-center">Welcome to Techbrothers99</h2>`,
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
